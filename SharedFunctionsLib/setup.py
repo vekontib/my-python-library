96a3c072ef0c7c99fcd906fd928ab9aa57ac9604
vekontib <54058286+vekontib@users.noreply.github.com>
2022-01-19 21:37:34 +0100

    Update setup.py

--- SharedFunctionsLib/setup.py
+++ SharedFunctionsLib/setup.py
@@ -1,1 +1,11 @@
+from setuptools import find_packages, setup
 
+setup(
+    name='SharedFunctionsLib',
+    packages=find_packages(include=['SharedFunctionsLib']),
+    version='0.0.1',
+    description='Demo Python library creation',
+    author='Tibor Vekony',
+    license='MIT',
+    install_requires=[]
+)
